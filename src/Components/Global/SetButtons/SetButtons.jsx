39f096ba9f7a13ab4e1a862d98d37a42ea3f9561
Vitaliy Tikhonenko <113926273+TikhonenkoVV@users.noreply.github.com>
2026-01-04 17:44:25 +0100

    add user avatar in header and in burger

--- src/Components/Global/SetButtons/SetButtons.jsx
+++ src/Components/Global/SetButtons/SetButtons.jsx
@@ -1,15 +1,15 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { LangMenu } from './Components/LangMenu/LangMenu';
 import { LangBtn, SetButtonWrapper, UserBtn } from './SetButtons.styled';
 import {
   FLAG_DE_URL,
   FLAG_UA_URL,
   FLAG_UK_URL,
+  IMAGE_CLOUD_URL,
   LOCAL_DE,
   LOCAL_EN,
   PATCH_OPERATION
 } from 'utils/GlobalUtils';
-import { selectUser } from 'store/auth/selectors';
 import { useProductState } from 'hooks/useProductState';
 import { useModal } from 'hooks/useModal';
 import { useClickOutsideModal } from 'hooks/useClickOutsideModal';
@@ -18,9 +18,10 @@ import { Loader } from '../Loader/Loader';
 import { SvgIcon } from '../SvgIcon/SvgIcon';
 import { Modal } from '../Modal/Modal';
 import { Profile } from 'Components/Header/Components/Profile/Profile';
+import { useAuth } from 'hooks/useAuth';
 
 export const SetButtons = () => {
-  const { locale } = useSelector(selectUser);
+  const { locale, avatarURL } = useAuth();
   const { isLoading, operation } = useProductState(null, PATCH_OPERATION);
   const dispatch = useDispatch();
 
@@ -69,7 +70,14 @@ export const SetButtons = () => {
           />
         </LangBtn>
         <UserBtn type="button" onClick={openProfileModal}>
-          <SvgIcon w={36} h={36} icon={'avatar'} aria-label="icon user" />
+          {avatarURL === '' ? (
+            <SvgIcon w={36} h={36} icon={'avatar'} aria-label="icon user" />
+          ) : (
+            <img
+              src={`${IMAGE_CLOUD_URL}w_36,h_36,c_fill/${avatarURL}`}
+              alt="user-avatar"
+            />
+          )}
         </UserBtn>
       </SetButtonWrapper>
       {isProfileModalOpen && (
--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -12,6 +12,7 @@ export const useAuth = () => {
   const isRefreshing = useSelector(selectIsRefreshing);
   const user = useSelector(selectUser);
   const { locale } = user;
+  const { avatarURL } = user;
   const shortLocale =
     locale === 'en-UK' ? 'en' : locale === 'de-DE' ? 'de' : 'ua';
   const authError = useSelector(selectAuthError);
@@ -22,6 +23,7 @@ export const useAuth = () => {
     isRefreshing,
     user,
     locale,
+    avatarURL,
     shortLocale,
     authError,
     isRegistered
